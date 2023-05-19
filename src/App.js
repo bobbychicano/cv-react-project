d2703618c29e28b79e0207dd702d98eaca70cd32
Robert 440679 <rescobedo12@gmail.com>
2023-05-19 12:14:17 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import React, { Component, useState } from "react";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default App;
