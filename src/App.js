04c911abd198dcc07b99e54d54f47dc3f7281132
Robert 440679 <rescobedo12@gmail.com>
2023-05-20 09:50:31 -0700

    Set up initial file structure and components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import "./App.css";
 import React, { Component, useState } from "react";
+import Introduction from "./Components/Introduction.js";
+import Education from "./Components/Introduction.js";
+import Experience from "./Components/Introduction.js";
 
 class App extends Component {
   constructor() {
@@ -9,7 +12,13 @@ class App extends Component {
   }
 
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <Introduction />
+        <Education />
+        <Experience />
+      </div>
+    );
   }
 }
 
